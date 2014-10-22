60e358316b64844ccf6906a66e8e49360fe2ac15
zsd <zsd12379@sina.com>
2014-10-22 22:20:50 +0800

    输出时间的测试
    输出时间的测试

---
+++ src/com/zsd/PrintTimeTest.java
@@ -0,0 +1,19 @@
+package com.zsd;
+
+/**
+ * 时间输出测试.
+ */
+public class PrintTimeTest {
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis()); // 毫秒
+        System.out.println(System.nanoTime()); // 纳秒
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+    }
+}
