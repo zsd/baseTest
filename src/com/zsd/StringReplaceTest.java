4a5bda35f7ad679bb936d3ef363fd2288c9cc9a8
zsd <zsd12379@sina.com>
2015-03-20 08:42:23 +0800

    字符串替换测试
    字符串替换测试

--- src/com/zsd/StringReplaceTest.java
+++ src/com/zsd/StringReplaceTest.java
@@ -16,6 +16,34 @@ public class StringReplaceTest {
         System.out.println("加密数据：" + result);
     }
 
+    /**
+     * 地址加密.
+     * 某某路6号楼1单元702加密成 **6号**1单**702
+     */
+    public void replaceAddress() {
+        String address = "某某路6号楼1单元702";
+        char[] tempArr = address.toCharArray();
+        String result = "";
+        int preType = 0; // 上一个字符类型，默认中文 0 中文、1 数字、字符
+        for (int i = 0; i < tempArr.length; i++) {
+            int temp = (int) tempArr[i];
+            if (temp >= 19968 && temp <= 171941) { // 汉字
+                if (preType == 1) {
+                    result = result + tempArr[i];
+                }
+                preType = 0;
+            } else {
+                if (preType == 0) {
+                    result = result + "**" + tempArr[i];
+                } else if (preType == 1) {
+                    result = result + tempArr[i];
+                }
+                preType = 1;
+            }
+        }
+        System.out.println("加密数据：" + result);
+    }
+
     public static void main(String[] args) {
 
         /**
@@ -27,5 +55,6 @@ public class StringReplaceTest {
 
         StringReplaceTest st = new StringReplaceTest();
         st.subPhone();
+        st.replaceAddress();
     }
 }
