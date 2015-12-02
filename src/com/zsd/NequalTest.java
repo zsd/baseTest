a3b0a9e620e12217df7d5d0890082d6dad79f7b3
zsd <zsd12379@sina.com>
2015-12-02 15:10:01 +0800

    知乎问题 n = n = n + 1 的n最终结果的测试.

---
+++ src/com/zsd/NequalTest.java
@@ -0,0 +1,14 @@
+package com.zsd;
+
+/**
+ * n等于几的代码测试.
+ * @author zsd
+ */
+public class NequalTest {
+
+    public static void main(String args[]) {
+        int n = 0;
+        n = n = n + 1;
+        System.out.println("n = " + n);
+    }
+}
